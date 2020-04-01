93b9a5027deda09c527c1445cbf614eda778daef
MikeDev505 <mikedev505@gmail.com>
2020-04-01 23:23:51 +0200

    init

---
+++ Cvl.NodeNetwork/Cvl.NodeNetwork.WebHub/Controllers/NodeNetworkController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Cvl.NodeNetwork.WebHub.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NodeNetworkController
+    {
+        [HttpPost]
+        [HttpGet]
+        public string Get(string message)
+        {
+            return "ping " + message;
+        }
+    }
+}
---
+++ Cvl.NodeNetwork/Cvl.NodeNetwork.WebHub/SignalR/NodeNetworkHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cvl.NodeNetwork.WebHub.SignalR
+{
+    public class NodeNetworkHub : Hub
+    {
+        public Task SendMessage(string user, string message)
+        {
+
+            return Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
